4358fa39a854c585bffe5b0008e8428345d2233d
Samar <osmansamar367@gmail.com>
2025-02-18 17:06:11 +0100

    Added footer

--- footer.php
+++ footer.php
@@ -1,14 +1,30 @@
 <footer class="footer">
     <style>
-    .footer-section {}
+    .footer-section {
+        width: 100%;
+        max-width: 1700px;
+    }
+
+    .left-cont {
+        /* background: rgb(77, 242, 1) 0% 0% no-repeat padding-box; */
+        /* left: 140px; */
+        /* width: 1140px; */
+        height: 442px;
+    }
+
+    .right-cont {
+        background: #F29401 0% 0% no-repeat padding-box;
+        /* width: 630px; */
+        height: 410px;
+    }
     </style>
 
-
+    <!-- offset-md-1 -->
     <div class="footer-section">
-        <div class="container">
+        <div class="">
             <div class="row">
-                <div class="col-12 col-lg-6"></div>
-                <div class="col-12 col-lg-6"></div>
+                <div class="col-12 col-lg-6  left-cont"></div>
+                <div class="col-12 col-lg-6  right-cont"></div>
             </div>
         </div>
     </div>
