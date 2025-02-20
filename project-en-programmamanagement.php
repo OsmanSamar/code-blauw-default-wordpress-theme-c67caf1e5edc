c61688df603d25b7a0b75c47f3f7da1e5b88379c
Samar <osmansamar367@gmail.com>
2025-02-20 23:09:33 +0100

    Added blauwblock

--- project-en-programmamanagement.php
+++ project-en-programmamanagement.php
@@ -124,6 +124,130 @@
     }
 
 
+    .testimonial-slide {
+        background: #FFFFFF 0% 0% no-repeat padding-box;
+        border-radius: 5px;
+        padding: 18px;
+        box-shadow: 10px 10px 60px #2072BE1A;
+        text-align: left;
+        max-width: 400px;
+        margin: auto;
+        position: relative;
+        z-index: 3;
+
+    }
+
+    .testimonial-text {
+        font-size: 16px;
+        font-weight: 300;
+        margin-bottom: 8px;
+        font-family: 'Scala Sans Pro', sans-serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+    }
+
+    .testimonial-naam {
+        font-family: 'Scala Sans Pro', sans-serif;
+        font-size: 16px;
+        font-weight: 600;
+        color: #1e73be;
+        opacity: 1;
+        letter-spacing: 0px;
+    }
+
+    .swiper-container {
+        /* position: relative; */
+        width: 100%;
+        margin: 0 auto;
+        overflow: clip;
+        overflow-y: visible;
+
+    }
+
+    .swiper-container .swiper-pagination {
+        bottom: -50px !important;
+    }
+
+    .swiper-container .swiper-pagination-bullet {
+        width: 10px;
+        height: 10px;
+        background-color: #FFFFFF;
+    }
+
+    .swiper-header {
+        display: flex;
+        align-items: baseline;
+        justify-content: space-between;
+        position: absolute;
+        top: -70px;
+        right: 0;
+        width: 100%;
+    }
+
+    .swiper-text {
+        font-size: 16px;
+        font-weight: 300;
+        white-space: nowrap;
+        text-align: left;
+        line-height: 50px;
+        font-family: 'Richmond Display', serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+    }
+
+    .swiper-button-prev,
+    .swiper-button-next {
+        /* position: static !important; */
+        position: relative !important;
+        width: 41px;
+        height: 41px;
+        border: 1px solid #132030;
+        color: #132030;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 10;
+
+    }
+
+
+    .swiper-button-next::after,
+    .swiper-rtl .swiper-button-prev::after {
+        font-size: 16px;
+        font-weight: 900;
+    }
+
+    .swiper-button-prev:after,
+    .swiper-rtl .swiper-button-next:after,
+    .swiper-button-next::after,
+    .swiper-rtl .swiper-button-prev::after {
+        font-size: 16px;
+        font-weight: 900;
+    }
+
+
+    .swiper-container .swiper-button-prev {
+        position: absolute;
+        left: 82% !important;
+        top: -70px !important;
+    }
+
+    .swiper-container .swiper-button-next {
+        position: absolute;
+        right: 5px !important;
+        top: -12px !important;
+    }
+
+    .arrow {
+        background-color: #1E73BE;
+        border-radius: 50px;
+        height: 28px;
+        width: 28px;
+    }
+
 
     @media (min-width: 992px) {
         .pro-hero-title {
@@ -135,6 +259,16 @@
         b,
         strong {
             font-size: inherit;
+        }
+
+        .swiper-container .swiper-button-prev {
+            position: absolute;
+            left: -12% !important;
+            top: -12px !important;
+        }
+
+        .swiper-text {
+            font-size: 36px;
         }
 
 
@@ -248,6 +382,74 @@
 
 
 
+        <!--first-blue-block  -->
+        <div class="first-blue-block position-relative">
+            <div class="container blue-container">
+                <div class="swiper-container swiper position-relative ">
+
+                    <div class="swiper-header">
+                        <div class="swiper-text">
+                            <?= get_field("onzesuccesverhalen") ?>
+                        </div>
+                        <div class="d-flex align-items-center gap-2">
+                            <div class="swiper-button-prev"></div>
+                            <div class="swiper-button-next"></div>
+                        </div>
+                    </div>
+
+                    <div class="swiperwerken werk-swiper ">
+                        <div class="swiper-wrapper">
+                            <?php
+                            $delay = 0;
+                            $posts = get_posts([
+                                'post_type' => 'werken_voor',
+                                'numberposts' => -1,
+                                "order" => 'asc'
+                            ]);
+
+                            foreach ($posts as $post) {
+                                $fields = get_fields($post->ID);
+                                ?>
+                            <div class="swiper-slide d-flex flex-column h-100">
+                                <a href="<?= get_permalink($post) ?>" class="d-flex flex-column h-100">
+                                    <div class="testimonial-slide d-flex flex-column h-100">
+                                        <img src="<?= $fields["backimg"]['url'] ?>"
+                                            alt="<?= $fields['backimg']['alt'] ?>" class="card-img-top"
+                                            style="height: 101px; border-radius: 5px; background: #F7F6F4; object-fit: scale-down;" />
+
+                                        <div class="card-body d-flex flex-column  mt-1 mb-0 pb-4 pt-2  h-100">
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <span class="card-text"><?= $post->post_title ?></span>
+                                                <span class="d-flex justify-content-center align-items-center arrow">
+                                                    <img src="<?= get_template_directory_uri() ?>/images/whitenextarrow.svg"
+                                                        alt="go to article" class="icon" />
+                                                </span>
+                                            </div>
+                                            <span class="card-title mt-auto">
+                                                <span><?= $fields["text"] ?></span>
+                                            </span>
+                                        </div>
+
+
+                                    </div>
+                                </a>
+                            </div>
+                            <?php
+                                $delay += 300;
+                            }
+                            ?>
+                        </div>
+                    </div>
+
+                    <div class="swiper-pagination"></div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+
 
 
 
@@ -257,6 +459,62 @@
 
 
     </div>
+
+
+    <script>
+    let swiperwerken = new Swiper('.werk-swiper', {
+        slidesPerView: 2.5,
+        grabCursor: true,
+        spaceBetween: 20,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+
+        breakpoints: {
+            360: {
+                slidesPerView: 1.3
+            },
+            540: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 2.5
+            },
+            992: {
+                slidesPerView: 3.2
+            },
+            1025: {
+                slidesPerView: 4
+            },
+        },
+
+        // R ein itia li ze A O S after Swiper initialization
+        on: {
+            init: function() {
+                AOS.refresh();
+            },
+            slideChangeTransitionEnd: function() {
+                AOS.refresh();
+            }
+
+        }
+
+
+    });
+
+    // I nitialize AOS
+    AOS.init({
+        duration: 800, // Animation duration
+        once: true, // Only animate once
+    });
+    </script>
 </main>
 
 
