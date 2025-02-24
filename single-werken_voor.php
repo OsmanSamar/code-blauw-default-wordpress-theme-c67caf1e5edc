582d43f2843408b4a4061199631ea064051aef25
Samar <osmansamar367@gmail.com>
2025-02-24 16:52:20 +0100

    Updated post page is good

--- single-werken_voor.php
+++ single-werken_voor.php
@@ -39,21 +39,21 @@
         <div class="medium-container">
             <div class="container">
                 <div class="row">
-                    <div class="col-12 col-lg-5  mb-3 " data-aos="fade-right" data-aos-offset="100" data-aos-delay="50"
+                    <div class="col-12 col-lg-5   mb-3 " data-aos="fade-right" data-aos-offset="100" data-aos-delay="50"
                         data-aos-duration="1000" data-aos-easing="ease-in-out">
                         <div class="d-flex flex-column  text-left gap-4">
-                            <span class="left-title">
+                            <span class="post-werk-title">
                                 <?= get_field("lefttitle") ?>
                             </span>
-                            <span class="left-">
+                            <span class="post-werk-left">
                                 <?= get_field("lefttext") ?>
                             </span>
 
                         </div>
                     </div>
-
-                    <div class="col-12 col-lg-5 offset-lg-1" data-aos="fade-left" data-aos-offset="100"
-                        data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out">
+                    <div class="col-lg-1 "></div>
+                    <div class="col-12 col-lg-6 " data-aos="fade-left" data-aos-offset="100" data-aos-delay="50"
+                        data-aos-duration="1000" data-aos-easing="ease-in-out">
                         <img src="<?= get_field("mediumimg")['url'] ?>" alt="<?= get_field("")['alt'] ?>"
                             class="medium-img" />
                     </div>
@@ -65,42 +65,48 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-lg-6 col-md-6 mb-3">
-                        <span class="whitecontainertext">
+                        <p class="whitecontainertext-right">
                             <?= get_field("seclefttext") ?>
-                        </span>
+                        </p>
                     </div>
                     <div class="col-12 col-lg-6 col-md-6">
-                        <span class="whitecontainertext">
+                        <p class="whitecontainertext-left">
                             <?= get_field("righttext") ?>
-                        </span>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+
+
 
 
         <!--White-containe-->
-        <div class="small-whit-container ">
-            <div class="whi-container  " data-aos="fade-up" data-aos-offset="100" data-aos-delay="50"
-                data-aos-duration="1000" data-aos-easing="ease-in-out">
-                <div class="d-flex align-items-center gap-4 flex-column flex-lg-row">
-                    <span class="organ-title">
-                        Ook de volgende stap zetten met jouw organisatie?
-                        <?= get_field("organisatietext") ?>
-                    </span>
+        <div class="container" style="margin-bottom: 90px;">
+            <div class="small-whit-container">
+                <div class="whi-container  " data-aos="fade-up" data-aos-offset="100" data-aos-delay="50"
+                    data-aos-duration="1000" data-aos-easing="ease-in-out">
+                    <div class="d-flex align-items-center gap-4 flex-column flex-lg-row">
+                        <span class="organ-title">
+                            Ook de volgende stap zetten met jouw organisatie?
+                            <?= get_field("organisatietext") ?>
+
+                        </span>
 
 
-                    <div class="circle-border gap-1  btn-primary-custom " style=" width: fit-content;">
-                        <!-- width:195px -->
-                        <a h ref="<?= get_field("vraagaanbtn")['url'] ?>" class="btn btn-primary">
-                            <?= get_field("vraagaanbtn")['title'] ?>
-                        </a>
-                        <img src="<?= get_template_directory_uri() ?>/images/whitenextarrow.svg" alt="go to article"
-                            class="testimonial-arrow" />
+                        <div class="circle-border gap-1  btn-primary-custom " style=" width: fit-content;">
+                            <!-- width:195px -->
+                            <a h ref="<?= get_field("vraagaanbtn")['url'] ?>" class="btn btn-primary">
+                                <?= get_field("vraagaanbtn")['title'] ?>
+                            </a>
+                            <img src="<?= get_template_directory_uri() ?>/images/whitenextarrow.svg" alt="go to article"
+                                class="testimonial-arrow" />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!--End of wshite-containe-->
 
     </div>
 
