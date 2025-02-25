578885e29b087b4a12b9609b987b48fef78dc8a6
Samar <osmansamar367@gmail.com>
2025-02-25 10:18:46 +0100

    Updated

--- kennismaken.php
+++ kennismaken.php
@@ -4,175 +4,175 @@
 
 
     <style>
-        .hero-section {
-            background: url("<?= get_field('heroimage')['url'] ?> ");
-            width: 100%;
-            max-width: 1700px;
-            margin: 0 auto;
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            object-fit: cover;
-        }
+    .hero-section {
+        background: url("<?= get_field('heroimage')['url'] ?> ");
+        width: 100%;
+        max-width: 1700px;
+        margin: 0 auto;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        object-fit: cover;
+    }
 
-        .kennis-hero-title {
-            font-size: 25px;
-            line-height: 76px;
-            font-weight: 500;
-            font-family: "Richmond Display", serif;
-            text-align: center;
-            letter-spacing: 0px;
-            color: #feffff;
-        }
+    .kennis-hero-title {
+        font-size: 25px;
+        line-height: 76px;
+        font-weight: 500;
+        font-family: "Richmond Display", serif;
+        text-align: center;
+        letter-spacing: 0px;
+        color: #feffff;
+    }
 
-        .kennis-hero-text {
-            text-align: center;
-            font-size: 14px;
-            line-height: 24px;
-            font-weight: 300;
-            font-family: "Libre Franklin", sans-serif;
-            letter-spacing: 1px;
-            color: #ffffff;
-            opacity: 1;
-            /* width: 505px; */
-            /* display: block; */
-            margin-bottom: 10px;
-        }
+    .kennis-hero-text {
+        text-align: center;
+        font-size: 14px;
+        line-height: 24px;
+        font-weight: 300;
+        font-family: "Libre Franklin", sans-serif;
+        letter-spacing: 1px;
+        color: #ffffff;
+        opacity: 1;
+        /* width: 505px; */
+        /* display: block; */
+        margin-bottom: 10px;
+    }
 
 
 
-        .blue-block {
-            width: 100%;
-            max-width: 1700px;
-            margin: 0 auto;
-            background-color: #CCE1F4;
-            padding-top: 50px;
-            padding-bottom: 50px;
-        }
+    .blue-block {
+        width: 100%;
+        max-width: 1700px;
+        margin: 0 auto;
+        background-color: #CCE1F4;
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
 
-        .waar-kunnen-helpen-title {
-            text-align: left;
-            font-size: 36px;
-            line-height: 50px;
-            font-weight: 300;
-            font-family: "Richmond Display", serif;
-            letter-spacing: 0px;
-            color: #132030;
-            opacity: 1;
-            margin-top: 50px;
-        }
+    .waar-kunnen-helpen-title {
+        text-align: left;
+        font-size: 36px;
+        line-height: 50px;
+        font-weight: 300;
+        font-family: "Richmond Display", serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+        margin-top: 50px;
+    }
 
-        .waar-kunnen-helpen-text {
+    .waar-kunnen-helpen-text {
 
-            text-align: left;
-            font-size: 16px;
-            line-height: 30px;
-            font-weight: 300;
-            font-family: 'Scala Sans Pro', sans-serif;
-            letter-spacing: 0px;
-            color: #132030;
-            opacity: 1;
-            letter-spacing: 0px;
-            text-wrap: balance;
-            /* word-break: break-word;
+        text-align: left;
+        font-size: 16px;
+        line-height: 30px;
+        font-weight: 300;
+        font-family: 'Scala Sans Pro', sans-serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+        letter-spacing: 0px;
+        text-wrap: balance;
+        /* word-break: break-word;
         white-space: normal; */
 
-        }
+    }
 
-        .contact-title {
+    .contact-title {
+        text-align: left;
+        font-size: 16px;
+        line-height: 30px;
+        font-weight: bold;
+        font-family: 'Scala Sans Pro', sans-serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+    }
+
+    .contact-info {
+        text-align: left;
+        font-size: 16px;
+        line-height: 30px;
+        font-weight: 300;
+        font-family: 'Scala Sans Pro', sans-serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+    }
+
+    /* Form Style */
+
+    .contact-form {
+        width: 100%;
+        max-width: 490px;
+        background: #ffffff 0% 0% no-repeat padding-box;
+        border-radius: 5px;
+        opacity: 1;
+        padding: 40px;
+    }
+
+    .form-1 .gform_wrapper .gform_body .gform_fields .gfield {
+        width: 100%;
+    }
+
+    /* Input field */
+    .gform-theme--foundation .gfield input,
+    .gform-theme--foundation .gfield textarea,
+    .gform-theme--foundation .gfield select {
+        inline-size: 100% !important;
+        box-sizing: border-box !important;
+        border-radius: 5px !important;
+        margin-bottom: 5px !important;
+        border: 1px solid #cce1f4 !important;
+    }
+
+    .gform_wrapper .gform_footer .gform_button {
+        width: 116px !important;
+        padding: 12px 21px !important;
+        align-items: center;
+        border-radius: 5px !important;
+        background-color: #f29401 !important;
+        color: #fff !important;
+        border: none !important;
+        display: inline-flex !important;
+        align-items: center !important;
+        background: url("<?= get_field('white_arrow')['url'] ?> ");
+        background-size: 9px 8px !important;
+        background-position: right 5px center !important;
+        background-repeat: no-repeat !important;
+    }
+
+    .gform-theme--framework,
+    .gform-field-label,
+    .gform-label,
+    .gfield_label,
+    .name-label {
+        color: #132030 !important;
+        letter-spacing: 0px !important;
+        font-size: 16px !important;
+        font-weight: bold !important;
+        line-height: 30px !important;
+    }
+
+    /* Form Style */
+
+
+    @media (min-width: 992px) {
+
+        .kennis-hero-title {
+            font-size: 50px;
             text-align: left;
+            line-height: 80px;
+        }
+
+
+        .kennis-hero-text {
             font-size: 16px;
-            line-height: 30px;
-            font-weight: bold;
-            font-family: 'Scala Sans Pro', sans-serif;
-            letter-spacing: 0px;
-            color: #132030;
-            opacity: 1;
+            width: 505px;
+            display: block;
         }
-
-        .contact-info {
-            text-align: left;
-            font-size: 16px;
-            line-height: 30px;
-            font-weight: 300;
-            font-family: 'Scala Sans Pro', sans-serif;
-            letter-spacing: 0px;
-            color: #132030;
-            opacity: 1;
-        }
-
-        /* Form Style */
-
-        .contact-form {
-            width: 100%;
-            max-width: 490px;
-            background: #ffffff 0% 0% no-repeat padding-box;
-            border-radius: 5px;
-            opacity: 1;
-            padding: 40px;
-        }
-
-        .form-1 .gform_wrapper .gform_body .gform_fields .gfield {
-            width: 100%;
-        }
-
-        /* Input field */
-        .gform-theme--foundation .gfield input,
-        .gform-theme--foundation .gfield textarea,
-        .gform-theme--foundation .gfield select {
-            inline-size: 100% !important;
-            box-sizing: border-box !important;
-            border-radius: 5px !important;
-            margin-bottom: 5px !important;
-            border: 1px solid #cce1f4 !important;
-        }
-
-        .gform_wrapper .gform_footer .gform_button {
-            width: 116px !important;
-            padding: 12px 21px !important;
-            align-items: center;
-            border-radius: 5px !important;
-            background-color: #f29401 !important;
-            color: #fff !important;
-            border: none !important;
-            display: inline-flex !important;
-            align-items: center !important;
-            background: url("<?= get_field('white_arrow')['url'] ?> ");
-            background-size: 9px 8px !important;
-            background-position: right 5px center !important;
-            background-repeat: no-repeat !important;
-        }
-
-        .gform-theme--framework,
-        .gform-field-label,
-        .gform-label,
-        .gfield_label,
-        .name-label {
-            color: #132030 !important;
-            letter-spacing: 0px !important;
-            font-size: 16px !important;
-            font-weight: bold !important;
-            line-height: 30px !important;
-        }
-
-        /* Form Style */
-
-
-        @media (min-width: 992px) {
-
-            .kennis-hero-title {
-                font-size: 50px;
-                text-align: left;
-                line-height: 80px;
-            }
-
-
-            .kennis-hero-text {
-                font-size: 16px;
-                width: 505px;
-                display: block;
-            }
-        }
+    }
     </style>
     <div>
 
@@ -237,13 +237,6 @@
                         <div class="d-flex flex-column contact-form form-1">
                             <?= str_replace(['__START__', '__END__'], ['<div class="form-col">', '</div>'], do_shortcode('[gravityform id="1" title="false" description="false"   cssClass="form-1"]')) ?>
                         </div>
-
-
-
-
-
-
-
                     </div>
                 </div>
             </div>
--- over-ons.php
+++ over-ons.php
@@ -2,148 +2,148 @@
 
 <main class="overons">
     <style>
-        .hero-section {
-            background: url("<?= get_field('heroimage')['url'] ?> ");
-            width: 100%;
-            max-width: 1700px;
-            margin: 0 auto;
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            object-fit: cover;
-        }
+    .hero-section {
+        background: url("<?= get_field('heroimage')['url'] ?> ");
+        width: 100%;
+        max-width: 1700px;
+        margin: 0 auto;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        object-fit: cover;
+    }
 
-        /*  */
-        .testimonial-slide {
-            background: #FFFFFF 0% 0% no-repeat padding-box;
-            border-radius: 5px;
-            padding: 18px;
-            box-shadow: 10px 10px 60px #2072BE1A;
-            text-align: left;
-            max-width: 400px;
-            margin: auto;
-            position: relative;
-            z-index: 3;
+    /*  */
+    .testimonial-slide {
+        background: #FFFFFF 0% 0% no-repeat padding-box;
+        border-radius: 5px;
+        padding: 18px;
+        box-shadow: 10px 10px 60px #2072BE1A;
+        text-align: left;
+        max-width: 400px;
+        margin: auto;
+        position: relative;
+        z-index: 3;
 
-        }
+    }
 
-        .testimonial-text {
-            font-size: 16px;
-            font-weight: 300;
-            margin-bottom: 8px;
-            font-family: 'Scala Sans Pro', sans-serif;
-            letter-spacing: 0px;
-            color: #132030;
-            opacity: 1;
-        }
+    .testimonial-text {
+        font-size: 16px;
+        font-weight: 300;
+        margin-bottom: 8px;
+        font-family: 'Scala Sans Pro', sans-serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+    }
 
-        .testimonial-naam {
-            font-family: 'Scala Sans Pro', sans-serif;
-            font-size: 16px;
-            font-weight: 600;
-            color: #1e73be;
-            opacity: 1;
-            letter-spacing: 0px;
-        }
+    .testimonial-naam {
+        font-family: 'Scala Sans Pro', sans-serif;
+        font-size: 16px;
+        font-weight: 600;
+        color: #1e73be;
+        opacity: 1;
+        letter-spacing: 0px;
+    }
 
-        .swiper-container {
-            width: 100%;
-            margin: 0 auto;
-            overflow: clip;
-            overflow-y: visible;
+    .swiper-container {
+        width: 100%;
+        margin: 0 auto;
+        overflow: clip;
+        overflow-y: visible;
 
-        }
+    }
 
-        .swiper-container .swiper-pagination {
-            bottom: -50px !important;
-        }
+    .swiper-container .swiper-pagination {
+        bottom: -50px !important;
+    }
 
-        .swiper-container .swiper-pagination-bullet {
-            width: 10px;
-            height: 10px;
-            background-color: #FFFFFF;
-        }
+    .swiper-container .swiper-pagination-bullet {
+        width: 10px;
+        height: 10px;
+        background-color: #FFFFFF;
+    }
 
-        .swiper-header {
-            display: flex;
-            align-items: baseline;
-            justify-content: space-between;
-            position: absolute;
-            top: -70px;
-            right: 0;
-            width: 100%;
+    .swiper-header {
+        display: flex;
+        align-items: baseline;
+        justify-content: space-between;
+        position: absolute;
+        top: -70px;
+        right: 0;
+        width: 100%;
 
-        }
+    }
 
-        .swiper-text {
-            font-size: 16px;
-            font-weight: 300;
-            white-space: nowrap;
-            text-align: left;
-            line-height: 50px;
-            font-family: 'Richmond Display', serif;
-            letter-spacing: 0px;
-            color: #132030;
-            opacity: 1;
-        }
+    .swiper-text {
+        font-size: 16px;
+        font-weight: 300;
+        white-space: nowrap;
+        text-align: left;
+        line-height: 50px;
+        font-family: 'Richmond Display', serif;
+        letter-spacing: 0px;
+        color: #132030;
+        opacity: 1;
+    }
 
-        .swiper-button-prev,
-        .swiper-button-next {
-            position: relative !important;
-            width: 41px;
-            height: 41px;
-            border: 1px solid #132030;
-            color: #132030;
-            border-radius: 5px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            z-index: 10;
+    .swiper-button-prev,
+    .swiper-button-next {
+        position: relative !important;
+        width: 41px;
+        height: 41px;
+        border: 1px solid #132030;
+        color: #132030;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 10;
 
-        }
-
-
-        .swiper-button-next::after,
-        .swiper-rtl .swiper-button-prev::after {
-            font-size: 16px;
-            font-weight: 900;
-        }
-
-        .swiper-button-prev:after,
-        .swiper-rtl .swiper-button-next:after,
-        .swiper-button-next::after,
-        .swiper-rtl .swiper-button-prev::after {
-            font-size: 16px;
-            font-weight: 900;
-        }
+    }
 
 
-        .swiper-container .swiper-button-prev {
-            position: absolute;
-            /* left: 82% !important;
+    .swiper-button-next::after,
+    .swiper-rtl .swiper-button-prev::after {
+        font-size: 16px;
+        font-weight: 900;
+    }
+
+    .swiper-button-prev:after,
+    .swiper-rtl .swiper-button-next:after,
+    .swiper-button-next::after,
+    .swiper-rtl .swiper-button-prev::after {
+        font-size: 16px;
+        font-weight: 900;
+    }
+
+
+    .swiper-container .swiper-button-prev {
+        position: absolute;
+        /* left: 82% !important;
         top: -70px !important; */
-            left: -2% !important;
-            top: -13px !important;
-        }
+        left: -2% !important;
+        top: -13px !important;
+    }
 
-        .swiper-container .swiper-button-next {
-            position: absolute;
-            right: 5px !important;
-            top: -12px !important;
-        }
+    .swiper-container .swiper-button-next {
+        position: absolute;
+        right: 5px !important;
+        top: -12px !important;
+    }
 
-        /* End of Swiper */
+    /* End of Swiper */
 
 
 
-        .container-block {
-            /* background-color: #F7F6F4; */
-            width: 100%;
-            max-width: 1700px;
-            margin: 0 auto;
-        }
+    .container-block {
+        /* background-color: #F7F6F4; */
+        width: 100%;
+        max-width: 1700px;
+        margin: 0 auto;
+    }
 
-        /* .whi-container {
+    /* .whi-container {
             background: #FFFFFF 0% 0% no-repeat padding-box;
             width: 1000px;
             position: relative;
@@ -161,7 +161,7 @@
             margin: 0 auto;
         } */
 
-        /* .organ-title {
+    /* .organ-title {
             font-size: 26px;
             line-height: 35px;
             font-weight: 300;
@@ -172,42 +172,42 @@
             opacity: 1;
         } */
 
-        @media (min-width: 768px) {
-            .swiper-container .swiper-button-prev {
-                position: absolute;
-                left: -3% !important;
-                top: -13px !important;
-            }
+    @media (min-width: 768px) {
+        .swiper-container .swiper-button-prev {
+            position: absolute;
+            left: -3% !important;
+            top: -13px !important;
+        }
 
+    }
+
+
+    @media (min-width: 992px) {
+
+        .swiper-container .swiper-button-prev {
+            position: absolute;
+            left: -12% !important;
+            top: -12px !important;
+        }
+
+        .swiper-text {
+            font-size: 36px;
+        }
+
+        .content-wrapper {
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
         }
 
 
-        @media (min-width: 992px) {
+        .first-blue-block {
+            height: 683px;
+            margin-top: unset;
+        }
 
-            .swiper-container .swiper-button-prev {
-                position: absolute;
-                left: -12% !important;
-                top: -12px !important;
-            }
-
-            .swiper-text {
-                font-size: 36px;
-            }
-
-            .content-wrapper {
-                padding: 20px;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-            }
-
-
-            .first-blue-block {
-                height: 683px;
-                margin-top: unset;
-            }
-
-            /* .whi-container {
+        /* .whi-container {
                 height: 131px;
             }
 
@@ -219,7 +219,7 @@
 
 
 
-        }
+    }
     </style>
     <div>
 
@@ -251,13 +251,14 @@
                 </div>
             </div> -->
             <!-- pointer-events: auto ;z-index: 1000 ; -->
-            <div class="circle-border btn-primary-custom width-fit-content" style="width: fit-content;">
+            <div class="circle-border btn-primary-custom">
                 <a href="<?= get_field("kennismakenbtn")['url'] ?>" class="btn btn-primary">
                     <?= get_field("kennismakenbtn")['title'] ?>
                 </a>
                 <img src="<?= get_field("arrow")['url'] ?>" alt="<?= get_field("arrow")['alt'] ?>"
-                    style="width: 9px; height: 7.69px; object-fit: cover; display: block; " />
+                    style="pointer-events: auto ;z-index: 1000 " />
             </div>
+
 
 
         </div>
@@ -394,34 +395,34 @@
                                 $fields = get_fields($post->ID);
                                 $testimonial = get_field("testimonialwerkvoor");
                                 ?>
-                                <div class="swiper-slide d-flex flex-column h-100">
-                                    <a href="<?= get_permalink($post) ?>" class=" d-flex flex-column h-100">
-                                        <div class="testimonial-slide d-flex flex-column h-100">
-                                            <img src="<?= $testimonial["logo"]['url'] ?>"
-                                                alt=" <?= $testimonial['logo']['alt'] ?>" class="card-img-top"
-                                                style="height: 101px; border-radius: 5px; background: #F7F6F4; object-fit: scale-down;" />
+                            <div class="swiper-slide d-flex flex-column h-100">
+                                <a href="<?= get_permalink($post) ?>" class=" d-flex flex-column h-100">
+                                    <div class="testimonial-slide d-flex flex-column h-100">
+                                        <img src="<?= $testimonial["logo"]['url'] ?>"
+                                            alt=" <?= $testimonial['logo']['alt'] ?>" class="card-img-top"
+                                            style="height: 101px; border-radius: 5px; background: #F7F6F4; object-fit: scale-down;" />
 
-                                            <div class="card-body d-flex flex-column  mt-1 mb-0 pb-4 pt-2  h-100">
-                                                <div class="d-flex justify-content-between align-items-center">
-                                                    <span class="card-text">
-                                                        <?= $post->post_title ?></span>
-                                                    <span class="d-flex justify-content-center align-items-center arrow">
-                                                        <!-- <img src="<?= get_template_directory_uri() ?>/images/whitenextarrow.svg"
+                                        <div class="card-body d-flex flex-column  mt-1 mb-0 pb-4 pt-2  h-100">
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <span class="card-text">
+                                                    <?= $post->post_title ?></span>
+                                                <span class="d-flex justify-content-center align-items-center arrow">
+                                                    <!-- <img src="<?= get_template_directory_uri() ?>/images/whitenextarrow.svg"
                                                         alt="go to article" class="icon" /> -->
-                                                        <img src="<?= $testimonial["white_arrow"]['url'] ?>"
-                                                            alt=" <?= $testimonial['white_arrow']['alt'] ?>"
-                                                            class="card-img-top"
-                                                            style="width: 9px; height: 7.69px; object-fit: cover; display: block; " />
-                                                    </span>
-                                                </div>
-                                                <span class="card-title mt-auto">
-                                                    <span><?= $testimonial["text"] ?></span>
+                                                    <img src="<?= $testimonial["white_arrow"]['url'] ?>"
+                                                        alt=" <?= $testimonial['white_arrow']['alt'] ?>"
+                                                        class="card-img-top"
+                                                        style="width: 9px; height: 7.69px; object-fit: cover; display: block; " />
                                                 </span>
                                             </div>
+                                            <span class="card-title mt-auto">
+                                                <span><?= $testimonial["text"] ?></span>
+                                            </span>
                                         </div>
-                                    </a>
-                                </div>
-                                <?php
+                                    </div>
+                                </a>
+                            </div>
+                            <?php
                                 $delay += 300;
                             }
                             ?>
@@ -484,62 +485,62 @@
 
 
     <script>
-        let newswiper = new Swiper('.werk-swiper2', {
-            slidesPerView: 2.5,
-            grabCursor: true,
-            spaceBetween: 20,
+    let newswiper = new Swiper('.werk-swiper2', {
+        slidesPerView: 2.5,
+        grabCursor: true,
+        spaceBetween: 20,
 
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        loop: true,
+        //  autoplay: { //added
+        //      delay: 2000, //added
+        //  },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+
+        breakpoints: {
+            360: {
+                slidesPerView: 1.3
             },
-            loop: true,
-            //  autoplay: { //added
-            //      delay: 2000, //added
-            //  },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+            540: {
+                slidesPerView: 2
             },
-
-
-            breakpoints: {
-                360: {
-                    slidesPerView: 1.3
-                },
-                540: {
-                    slidesPerView: 2
-                },
-                768: {
-                    slidesPerView: 2.5
-                },
-                992: {
-                    slidesPerView: 3.2
-                },
-                1025: {
-                    slidesPerView: 4
-                },
+            768: {
+                slidesPerView: 2.5
             },
+            992: {
+                slidesPerView: 3.2
+            },
+            1025: {
+                slidesPerView: 4
+            },
+        },
 
-            // R ein itia li ze A O S after Swiper initialization
-            on: {
-                init: function () {
-                    AOS.refresh();
-                },
-                slideChangeTransitionEnd: function () {
-                    AOS.refresh();
-                }
-
+        // R ein itia li ze A O S after Swiper initialization
+        on: {
+            init: function() {
+                AOS.refresh();
+            },
+            slideChangeTransitionEnd: function() {
+                AOS.refresh();
             }
 
+        }
 
-        });
 
-        // I nitialize AOS
-        AOS.init({
-            duration: 800, // Animation duration
-            once: true, // Only animate once
-        });
+    });
+
+    // I nitialize AOS
+    AOS.init({
+        duration: 800, // Animation duration
+        once: true, // Only animate once
+    });
     </script>
 
 </main>
