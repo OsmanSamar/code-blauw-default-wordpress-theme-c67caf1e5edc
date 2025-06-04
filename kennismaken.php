fcdd97fc058c87f043434d5c915f2a06d94d0774
Samar <osmansamar367@gmail.com>
2025-06-04 16:54:56 +0200

    Updated and clean up Kennismaken

--- kennismaken.php
+++ kennismaken.php
@@ -1,53 +1,39 @@
 <?php get_header()  //Template Name: kennismaken  ?>
 
 <main class="kennismaken">
-
-
-    <style>
-    
-
-    /* Form Style */
-    .gform_wrapper .gform_footer .gform_button {
-        background: url("<?= get_field('white_arrow')['url'] ?> ");
-    }
-    </style>
-    <div>
-
-
-        
+    <div class="container">
         <div>
             <?= get_template_part("components/hero") ?>
         </div>
-       
 
-        <div class="blue-block ">
-            <div class="container ">
+        <div class="blue-block position-relative">
+            <div class="inside-wrap ">
                 <div class="row container-form">
                     <div class="col-12 col-lg-6" data-aos="fade-right" data-aos-offset="100" data-aos-delay="50"
                         data-aos-duration="1000" data-aos-easing="ease-in-out">
                         <div class="d-flex flex-column text-align-flex-start gap-3 contact-left-col" style="   ">
-                            <span class="waar-kunnen-helpen-title">
+                            <h3 >
                                 <?= get_field("waarkunnenwemeehelpen") ?>
-                            </span>
-                            <span class="waar-kunnen-helpen-text">
+                            </h3>
+                            <span class="regular">
                                 <?= get_field("text") ?>
                             </span>
                             <div class="d-flex flex-column text-align-flex-start gap-2 mt-3 ">
-                                <span class="contact-title">
+                                <span class="text-bold regular  ">
                                     <?= get_field("contactmetblauw_ijs") ?>
                                 </span>
                                 <div class="d-flex flex-column mb-5 ">
                                     <a href="tel:<?= get_field("contact_number", 'options') ?>"
                                         class="d-flex flex-row align-items-center gap-2 contact-info ">
                                         <img src="<?= get_template_directory_uri() ?>/images/tele-icon.svg"
-                                            alt="go onze expertises" class="testimonial-arrow"
-                                            style="  width: 15px; height:15px;  object-fit: cover;" />
+                                            alt="go onze expertises" class="icon"
+                                             />
                                         <?= get_field("contact_number", 'options') ?>
                                     </a>
                                     <a href="mailto:<?= get_field("contact_mail", 'options') ?>"
                                         class="d-flex flex-row align-items-center gap-2 contact-info ">
                                         <img src="<?= get_template_directory_uri() ?>/images/mai-icon.svg" alt="go mail"
-                                            style="  width: 15px; height:15px;  object-fit: cover;" />
+                                         class="icon" />
                                         <?= get_field("contact_mail", 'options') ?>
                                     </a>
                                 </div>
