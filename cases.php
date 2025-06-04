638f6b388fb71abdd1041cf1eec026af6a167ccf
Samar <osmansamar367@gmail.com>
2025-06-04 14:53:57 +0200

    Clean up Cases page

--- cases.php
+++ cases.php
@@ -18,14 +18,14 @@ $fields = get_fields();
 
 
         <!--first-blue-block  -->
-        <div class="first-blue-block position-relative">
-            <div class=" blue-container">
+        <div class="blue-container position-relative">
+            <div class="inside-block">
                 <div class="row">
                     <div class="col-12 col-md-6 offset-md-3">
-                        <div class="section-header ">
-                            <div class="section-text">
+                        <div class="d-flex align-items-center justify-content-center ">
+                            <h3>
                                 <?= get_field("klantcases") ?>
-                            </div>
+                            </h3>
                         </div>
                     </div>
                 </div>
@@ -46,20 +46,18 @@ $fields = get_fields();
                             <a href="<?= get_permalink($post) ?>" class=" d-flex flex-column ">
                                 <div class=" card border-0 testimonial-slide d-flex flex-column ">
                                     <img src=" <?= $testimonial["logo"]['url'] ?>" alt=" <?= $testimonial['logo']['alt'] ?>"
-                                        class=" card-img-top"
-                                        style="height: 101px; border-radius: 5px; background: #F7F6F4 0% 0% no-repeat padding-box; object-fit: scale-down;" />
+                                        class="testimonial-img" />
 
                                     <div class="card-body mt-1 mb-0 pb-4 pt-2">
-                                        <!--  d-flex flex-column  -->
+                                       
                                         <div class="d-flex justify-content-between align-items-center">
-                                            <span class="regular"><?= $post->post_title ?></span>
+                                            <span class="testimonial-title"><?= $post->post_title ?></span>
                                             <span class="d-flex justify-content-center align-items-center arrow">
                                                 <img src="<?= $testimonial["white_arrow"]['url'] ?>"
-                                                    alt=" <?= $testimonial['white_arrow']['alt'] ?>" class="card-img-top"
-                                                    style="width: 9px; height: 7.69px; object-fit: cover; display: block;" />
+                                                    alt=" <?= $testimonial['white_arrow']['alt'] ?>" class="go-arrow"/>
                                             </span>
                                         </div>
-                                        <span class="card-title mt-auto" style=" color: #132030;">
+                                        <span class="testimonial-text mt-auto">
                                             <span>
                                                 <?= $testimonial["text"] ?></span>
                                         </span>
@@ -77,22 +75,15 @@ $fields = get_fields();
 
 
         </div>
-        <!--End of first-blue-block  -->
+       
         <!--White-containe-->
-        <div class="whi-container" data-aos="fade-up" data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000"
+        <div class="white-container" data-aos="fade-up" data-aos-offset="100" data-aos-delay="50" data-aos-duration="1000"
             data-aos-easing="ease-in-out">
             <div class="d-flex align-items-center gap-3 py-3 justify-content-between flex-column flex-lg-row">
-                <span class="organ-title">
+                <h3>
                     <?= get_field("organisatietext") ?>
-                    <!-- 
-                     <?php
-                     $organisatietext = get_field("organisatietext");
-                     echo '<pre>';
-                     print_r($organisatietext);
-                     echo '</pre>';
-                     ?> -->
-                </span>
-                <div class="" style="margin-right: 27px;">
+                </h3>
+                <div class="btn-left">
                     <a href="<?= get_field("vraageenbtn")['url'] ?>" class="button btn-primary">
                         <?= get_field("vraageenbtn")['title'] ?>
                         <img src="<?= get_template_directory_uri() ?>/images/whitenextarrow.svg" alt="go Contact page"
@@ -102,74 +93,9 @@ $fields = get_fields();
             </div>
         </div>
 
-
-        <!--End of wshite-containe-->
-
     </div>
 
 
-    <script>
-        let swiperwerken3 = new Swiper('.werk-swiper3', {
-            slidesPerView: 2.5,
-            grabCursor: true,
-            spaceBetween: 20,
-
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            // loop: true,
-            // autoplay: { //added
-            //      delay: 2000, //added
-            //  },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-
-
-            breakpoints: {
-                360: {
-                    slidesPerView: 1
-                },
-                410: {
-                    slidesPerView: 1.2
-
-                },
-                540: {
-                    slidesPerView: 2
-                },
-                768: {
-                    slidesPerView: 2.1
-                },
-                992: {
-                    slidesPerView: 3.2
-                },
-                1025: {
-                    slidesPerView: 4
-                },
-            },
-
-            // R ein itia li ze A O S after Swiper initialization
-            on: {
-                init: function () {
-                    AOS.refres h();
-                },
-                slideChangeTransitionEnd: function () {
-                    AOS.refresh();
-                }
-
-            }
-
-
-        });
-
-        // I nitialize AOS
-        AOS.init({
-            duration: 800, // Animation duration
-            once: true, // Only animate once
-        });
-    </script>
 
 
 
