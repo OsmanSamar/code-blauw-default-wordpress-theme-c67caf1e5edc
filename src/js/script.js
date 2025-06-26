3c8adedbc13a9ef717c11cec6ca4ec6b936eaf3c
Frank Blankespoor <frank@code-blauw.nl>
2025-06-26 11:49:36 +0200

    fontawesome not used

--- src/js/script.js
+++ src/js/script.js
@@ -4,16 +4,6 @@ import "bootstrap";
 // Styling
 import "../scss/style.scss";
 
-// Fontawesome
-import { dom, library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faFacebookF,
-  faTwitter,
-  faInstagram,
-  faLinkedinIn,
-} from "@fortawesome/free-brands-svg-icons";
-library.add(faFacebookF, faTwitter, faInstagram, faLinkedinIn);
-dom.watch();
 
 // General
 jQuery(document).ready(function ($) {
